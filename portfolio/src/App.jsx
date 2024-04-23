708c10f7b7d854e5a7c7441264c52f07b2ab9638
GayathriChalla22 <challagayathri2002@gmail.com>
2024-04-23 20:15:15 +0530

    initial commit

---
+++ portfolio/src/App.jsx
@@ -0,0 +1,27 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+function App() {
+
+  return (
+    <>
+        <ul class="nav nav-pills d-flex justify-content-end gap-4 mx-4 pt-4">
+  <li class="nav-item">
+    <a class="nav-link text-decoration-none text-secondary-emphasis" aria-current="page" href="#">About</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link text-decoration-none text-secondary-emphasis" href="#">Education</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link text-decoration-none text-secondary-emphasis" href="#">Skills</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link text-decoration-none text-secondary-emphasis">Projects</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link text-decoration-none text-secondary-emphasis">Contact</a>
+  </li>
+</ul>
+    </>
+  )
+}
+
+export default App
